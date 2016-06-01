9ee947334af17dbd641ac3146e8277cdda38674a
gaojiang1989 <gaojiang1989@gmail.com>
2016-06-01 17:38:06 +0900

    Revert "民宿项目"
    This reverts commit 5dc32c709af3de5f5ac456158212de1b33a72d8a.

--- src/com/sence/service/IUserManage.java
+++
@@ -1,9 +0,0 @@
-package com.sence.service;
-
-import com.sence.entity.User;
-
-public interface IUserManage {
-	public void addUser(User user);
-	public void modifyUser(User user);
-	public void deleteUser(User user);
-}
